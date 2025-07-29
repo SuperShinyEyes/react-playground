dfaf5afbe84b651d5b22cf02781207bd5bd92687
Seyoung Park <seyoung.arts.park@protonmail.com>
2025-07-29 18:16:40 +0300

    Add a table

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from './+types/home';
 import { Welcome } from '../welcome/welcome';
+// import { table } from 'console';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,6 +9,87 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+const data = [
+  { category: 'Fruits', price: '$1', stocked: true, name: 'Apple' },
+  { category: 'Fruits', price: '$1', stocked: true, name: 'Dragonfruit' },
+  { category: 'Fruits', price: '$2', stocked: false, name: 'Passionfruit' },
+  { category: 'Vegetables', price: '$2', stocked: true, name: 'Spinach' },
+  { category: 'Vegetables', price: '$4', stocked: false, name: 'Pumpkin' },
+  { category: 'Vegetables', price: '$1', stocked: true, name: 'Peas' },
+];
+
+function FilterableProductTable() {
+  return (
+    <div>
+      <SearchBar />
+      <ProductTable />
+    </div>
+  );
+}
+
+function ProductTable() {
+  const categories = [...new Set(data.map((x) => x.category))];
+  // console.log(categories);
+  const body = [];
+
+  let keyIndex = 0;
+  categories.forEach((c) => {
+    body.push(<ProductCategoryRow category={c} id={c} />);
+
+    data
+      .filter((e) => e.category === c)
+      .forEach((item) => {
+        console.log(`${c}: ${item.name}`);
+        body.push(
+          <ProductRow product={item.name} price={item.price} id={item.name} />
+        );
+        keyIndex++;
+      });
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{body}</tbody>
+    </table>
+  );
+}
+
+function SearchBar() {
+  return <input type="text" />;
+}
+
+function ProductCategoryRow({ category, id }) {
+  return (
+    <tr key={id}>
+      <td>{category}</td>
+    </tr>
+  );
+}
+
+function ProductRow({ product, price, id }) {
+  return (
+    <tr key={id}>
+      <td>{product}</td>
+      <td>{price}</td>
+    </tr>
+  );
+}
+
 export default function Home() {
-  return <Welcome />;
+  // return <Welcome />;
+  return (
+    <>
+      <h1>Hello, world!</h1>
+      <h2>Hello, world!</h2>
+      <h3>Hello, world!</h3>
+      <p>Hi</p>
+      <FilterableProductTable />
+    </>
+  );
 }
